7e01ef8d0fa60ce3c04df6cb23552e43730e6168
Meiirlan Serikbay <114663479+meiirsse@users.noreply.github.com>
2022-12-14 12:12:54 +0600

    Deploy

---
+++ springbootdemo/src/main/java/com/javadevkz/springbootdemo/service/UserService.java
@@ -0,0 +1,35 @@
+package com.javadevkz.springbootdemo.service;
+
+import com.javadevkz.springbootdemo.model.User;
+import com.javadevkz.springbootdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id){
+        return userRepository.getOne(id);
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id){
+        userRepository.deleteById(id);
+    }
+}
